7613c75ac05267c5653797de991453ddd0223a48
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-03-16 17:24:48 +0100

    Initial commit

--- app/src/main/java/com/example/myapplication/Server_withName.kt
+++ app/src/main/java/com/example/myapplication/Server_withName.kt
@@ -2,9 +2,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.provider.MediaStore.Video.Thumbnails.VIDEO_ID
+
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.youtube.player.YouTubeStandalonePlayer
+
 
 
 class Server_withName : AppCompatActivity()
